fdc51b77c5bef26fc2a199f479a3b49b146d60ea
Anxo <anxo@anxorl.es>
2017-09-06 22:49:16 +0200

    Export public clasess

--- src/LeafletGrid.ts
+++ src/LeafletGrid.ts
@@ -1,9 +1,14 @@
-import './control/L.Control.ColorBar'
-import './grid/Celda'
-import './grid/Malla'
-import './grid/MallaEscalar'
-import './grid/MallaVectorial'
-import './grid/Vector'
-import './layer/L.CanvasLayer.MallaEscalar'
-import './layer/L.CanvasLayer.MallaVectorial'
-import './layer/L.CanvasLayer.PuntosMalla'
+import { ColorBar } from './control/L.Control.ColorBar'
+import { Celda } from './grid/Celda'
+import { Malla } from './grid/Malla'
+import { MallaEscalar } from './grid/MallaEscalar'
+import { MallaVectorial } from './grid/MallaVectorial'
+import { Vector } from './grid/Vector'
+import { CanvasLayerMalla } from './layer/L.CanvasLayer.Malla'
+import { CanvasLayerMallaEscalar } from './layer/L.CanvasLayer.MallaEscalar'
+import { CanvasLayerMallaVectorial } from './layer/L.CanvasLayer.MallaVectorial'
+import { CanvasLayerPuntosMalla } from './layer/L.CanvasLayer.PuntosMalla'
+
+export { Celda, Vector, Malla, MallaEscalar, MallaVectorial }
+export { CanvasLayerMalla, CanvasLayerMallaEscalar, CanvasLayerMallaVectorial, CanvasLayerPuntosMalla }
+export { ColorBar }
