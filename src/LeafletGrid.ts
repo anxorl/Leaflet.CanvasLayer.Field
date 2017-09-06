ad240aa3806c879057114f085f8c48e961d7fbda
Anxo <anxo@anxorl.es>
2017-09-06 23:04:41 +0200

    More fixes

--- src/LeafletGrid.ts
+++ src/LeafletGrid.ts
@@ -1,14 +1,25 @@
-import { ColorBar } from './control/L.Control.ColorBar'
-import { Celda } from './grid/Celda'
-import { Malla } from './grid/Malla'
-import { MallaEscalar } from './grid/MallaEscalar'
-import { MallaVectorial } from './grid/MallaVectorial'
+import { ColorBar, IColorBarOptions } from './control/L.Control.ColorBar'
+import { Celda, ISizeCelda } from './grid/Celda'
+import { IMallaParams, Malla } from './grid/Malla'
+import { IMallaEscalarParams, MallaEscalar } from './grid/MallaEscalar'
+import { IMallaVectorialParams, MallaVectorial } from './grid/MallaVectorial'
 import { Vector } from './grid/Vector'
 import { CanvasLayerMalla } from './layer/L.CanvasLayer.Malla'
-import { CanvasLayerMallaEscalar } from './layer/L.CanvasLayer.MallaEscalar'
+import { CanvasLayerMallaEscalar, ICanvasLayerMallaEscalarOptions } from './layer/L.CanvasLayer.MallaEscalar'
 import { CanvasLayerMallaVectorial } from './layer/L.CanvasLayer.MallaVectorial'
 import { CanvasLayerPuntosMalla } from './layer/L.CanvasLayer.PuntosMalla'
 
+// Interfaces base
+export { ISizeCelda, IMallaParams, IMallaEscalarParams, IMallaVectorialParams }
+// Clases base
 export { Celda, Vector, Malla, MallaEscalar, MallaVectorial }
+
+// Interfaces de Layers
+export { ICanvasLayerMallaEscalarOptions }
+// Layers
 export { CanvasLayerMalla, CanvasLayerMallaEscalar, CanvasLayerMallaVectorial, CanvasLayerPuntosMalla }
+
+// Interfaces de ColorBar
+export { IColorBarOptions }
+// Colorbar
 export { ColorBar }
