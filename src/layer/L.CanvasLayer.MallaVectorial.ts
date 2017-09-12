3d0bac9846838074a1b4587a8c25b087ffb14cf3
Anxo <anxo@anxorl.es>
2017-09-12 18:24:05 +0200

    More patches

--- src/layer/L.CanvasLayer.MallaVectorial.ts
+++ src/layer/L.CanvasLayer.MallaVectorial.ts
@@ -70,7 +70,7 @@ export class CanvasLayerMallaVectorial extends CanvasLayerMalla<Vector> {
         const target = new LatLng(par.yt, par.xt)
 
         if (
-            viewInfo.layer._map &&
+            viewInfo.layer._map != null &&
             viewInfo.bounds.contains(source) &&
             par.age <= this.options.maxAge
         ) {
