0143a40761f8ff589ed17380176b114aa7a96d29
Anxo <anxo@anxorl.es>
2019-03-12 20:50:17 +0100

    Cambios:
    - Cámbianse cores e valores da escala LDEN
    - Permítense pintar valores por debaixo do mínimo da escala

--- src/colorscale/L.ColorScale.ts
+++ src/colorscale/L.ColorScale.ts
@@ -6,280 +6,288 @@
 import { Class, Util } from 'leaflet'
 
 export interface IScaleDefinition {
-    colors: string[]
-    positions?: number[]
+  colors: string[]
+  positions?: number[]
 }
 
 export interface IColorScaleOptions {
-    clampHigh?: boolean
-    clampLow: boolean
-    domain: number[]
-    height: number /* 1 is enough to get an array of colours */
-    width: number /* nº of steps = width */
+  clampHigh?: boolean
+  clampLow: boolean
+  domain: number[]
+  height: number /* 1 is enough to get an array of colours */
+  width: number /* nº of steps = width */
 }
 
 export class ColorScale extends Class {
 
-    public static buildFullDefinition(defTemp: IScaleDefinition): IScaleDefinition {
-        // Allows conversion from definition = ['color1', 'color2'...] to definition = {colors: ['color1', 'color2']}
-        let def: IScaleDefinition
-        (defTemp.colors.length > 0) ? def.colors = defTemp.colors : def = ColorScale.getScale(defTemp.colors[0])
+  public static buildFullDefinition(defTemp: IScaleDefinition): IScaleDefinition {
+    // Allows conversion from definition = ['color1', 'color2'...] to definition = {colors: ['color1', 'color2']}
+    let def: IScaleDefinition
+    (defTemp.colors.length > 0) ? def.colors = defTemp.colors : def = ColorScale.getScale(defTemp.colors[0])
 
-        // There are colors, but no positions (equally interval is assumed).
-        if (!def.positions) {
-            const nColors = def.colors.length
-            const delta = 1.0 / (nColors - 1)
-            const pos = []
-            let p = 0
-            do {
-                pos.push(p)
-                p = p + delta
-            } while (p <= 1.0)
-            def.positions = pos
-        }
-
-        return def
+    // There are colors, but no positions (equally interval is assumed).
+    if (!def.positions) {
+      const nColors = def.colors.length
+      const delta = 1.0 / (nColors - 1)
+      const pos = []
+      let p = 0
+      do {
+        pos.push(p)
+        p = p + delta
+      } while (p <= 1.0)
+      def.positions = pos
     }
 
-    public static getScale(name: string): IScaleDefinition {
-        return ColorScale.scales[name.toLowerCase()]
+    return def
+  }
+
+  public static getScale(name: string): IScaleDefinition {
+    return ColorScale.scales[name.toLowerCase()]
+  }
+
+  public static getScales(): string[] {
+    return Object.keys(ColorScale.scales)
+  }
+
+  public static rgbToHex(r: number, g: number, b: number): string {
+    const rgb = [r, g, b]
+
+    function _componentToHex(c: number) {
+      const hex = c.toString(16)
+      return hex.length === 1 ? '0' + hex : hex
     }
 
-    public static getScales(): string[] {
-        return Object.keys(ColorScale.scales)
+    return '#' + rgb.map(_componentToHex).join('')
+  }
+
+  public static hexToRgb(hexWithPrefix: string): number[] {
+    const hex = hexWithPrefix.slice(1)
+    const arrBuff = new ArrayBuffer(4)
+    const vw = new DataView(arrBuff)
+    vw.setUint32(0, parseInt(hex, 16), false)
+    const arrByte = new Uint8Array(arrBuff)
+
+    const r = arrByte[1]
+    const g = arrByte[2]
+    const b = arrByte[3]
+    return [r, g, b]
+  }
+
+  private static scales: { [n: string]: IScaleDefinition } = {
+    autumn: {
+      colors: ['#ff0000', '#ffff00'],
+      positions: [0, 1]
+    },
+    blackbody: {
+      colors: ['#00000000', '#e60000', '#e6d200', '#ffffff', '#a0c8ff'],
+      positions: [0, 0.2, 0.4, 0.7, 1]
+    },
+    bluered: {
+      colors: ['#0000ff', '#ff0000'],
+      positions: [0, 1]
+    },
+    bone: {
+      colors: ['#000000', '#545474', '#a9c8c8', '#ffffff'],
+      positions: [0, 0.376, 0.753, 1]
+    },
+    cool: {
+      colors: ['#00ffff', '#ff00ff'],
+      positions: [0, 1]
+    },
+    copper: {
+      colors: ['#000000', '#ffa066', '#ffc77f'],
+      positions: [0, 0.804, 1]
+    },
+    earth: {
+      colors: ['#000082', '#00b4b4', '#28d228', '#e6e632', '#784614', '#ffffff'],
+      positions: [0, 0.1, 0.2, 0.4, 0.6, 1]
+    },
+    electric: {
+      colors: ['#000000', '#1e0064', '#780064', '#a05a00', '#e6c800', '#fffadc'],
+      positions: [0, 0.15, 0.4, 0.6, 0.8, 1]
+    },
+    greens: {
+      colors: ['#00441b', '#006d2c', '#238b45', '#41ab5d', '#74c476', '#a1d99b', '#c7e9c0', '#e5f5e0', '#f7fcf5'],
+      positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+    },
+    greys: {
+      colors: ['#000000', '#ffffff'],
+      positions: [0, 1]
+    },
+    hot: {
+      colors: ['#000000', '#e60000', '#ffd200', '#ffffff'],
+      positions: [0, 0.3, 0.6, 1]
+    },
+    hsv: {
+      colors: ['#ff0000', '#fdff02', '#f7ff02', '#00fc04', '#00fc0a', '#01f9ff', '#0200fd', '#0800fd', '#ff00fb', '#ff00f5', '#ff0006'],
+      positions: [0, 0.169, 0.173, 0.337, 0.341, 0.506, 0.671, 0.675, 0.839, 0.843, 1]
+    },
+    ica: {
+      colors: ['#256814', '#40bd27', '#bfbf1f', '#8f201e', '#f40e05'],
+      positions: [0, 0.25, 0.5, 0.75, 1]
+    },
+    jet: {
+      colors: ['#000083', '#003CAA', '#05FFFF', '#FFFF00', '#FA0000', '#800000'],
+      positions: [0, 0.125, 0.375, 0.625, 0.875, 1]
+    },
+    lden: {
+      colors: ['#ffff02', '#ffcd69', '#ff8002', '#ff0202', '#ff00ff'],
+      positions: [55, 60, 65, 70, 75]
+    },
+    picnic: {
+      colors: ['#0000ff', '#3399ff', '#66ccff', '#99ccff', '#ccccff', '#ffffff', '#ffccff', '#ff99ff', '#ff66cc', '#ff6666', '#ff0000'],
+      positions: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    },
+    portland: {
+      colors: ['#0c3383', '#0a88ba', '#f2d338', '#f28f38', '#d91e1e'],
+      positions: [0, 0.25, 0.5, 0.75, 1]
+    },
+    rainbow: {
+      colors: ['#96005A', '#0000C8', '#0019FF', '#0098FF', '#2CFF96', '#97FF00', '#FFEA00', '#FF6F00', '#FF0000'],
+      positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+    },
+    rdbu: {
+      colors: ['#050aac', '#6a89f7', '#bebebe', '#dcaa84', '#e6915a', '#b20a1c'],
+      positions: [0, 0.35, 0.5, 0.6, 0.7, 1]
+    },
+    ruido: {
+      colors: ['#00ff0022', '#00ff00', '#008000', '#ffff00', '#a07f00', '#ff7f00', '#ff7f7f', '#ff0000', '#a00020', '#7f00a0', '#3f00ff'],
+      positions: [40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80]
+    },
+    spring: {
+      colors: ['#ff00ff', '#ffff00'],
+      positions: [0, 1]
+    },
+    summer: {
+      colors: ['#008066', '#ffff66'],
+      positions: [0, 1]
+    },
+    troposfera: {
+      colors: ['rgba(128,128,128,0)',
+        'rgb(79,9,207)',
+        'rgb(8,55,206)',
+        'rgb(8,184,216)',
+        'rgb(14,234,138)',
+        'rgb(20,229,9)',
+        'rgb(139,214,8)',
+        'rgb(248,193,26)',
+        'rgb(243,56,19)',
+        'rgb(219,9,103)'
+      ]
+    },
+    viridis: {
+      colors: ['#440154', '#482777', '#3f4a8a', '#31678e', '#26838f', '#1f9d8a', '#6cce5a', '#b6de2b', '#fee825']
+    },
+    winter: {
+      colors: ['#0000ff', '#00ff80'],
+      positions: [0, 1]
+    },
+    yignbu: {
+      colors: ['#081d58', '#253494', '#225ea8', '#1d91c0', '#41b6c4', '#7fcdbb', '#c7e9b4', '#edf8d9', '#ffffd9'],
+      positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+    },
+    yiorrd: {
+      colors: ['#800026', '#bd0026', '#e31a1c', '#fc4e2a', '#fd8d3c', '#feb24c', '#fed976', '#ffeda0', '#ffffcc'],
+      positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+    },
+  }
+
+  // private canvas: HTMLCanvasElement
+  private csImageData: Uint8ClampedArray
+  private definition: IScaleDefinition
+
+  private options: IColorScaleOptions = {
+    clampHigh: false,
+    clampLow: false,
+    domain: [0, 1],
+    height: 1,
+    width: 256,
+  }
+
+  constructor(definition: IScaleDefinition, options?: IColorScaleOptions) {
+    super()
+    Util.setOptions(this, options)
+
+    this.definition = ColorScale.buildFullDefinition(definition)
+    this._prepareScale()
+  }
+
+  public setDefinition(nome: string, stops: number[]) {
+    const definition = {
+      colors: ColorScale.scales[nome].colors,
+      positions: stops
+    }
+    this.definition = ColorScale.buildFullDefinition(definition)
+  }
+
+  public setDomain(d: number[]) {
+    this.options.domain = d
+    return this
+  }
+
+  public colorFor(value: number) {
+    const [min, max] = this.options.domain
+    const range = max - min
+
+    let c = Math.round((this.options.width - 1) * ((value - min) / range))
+
+    // clamp values?
+    let alpha = 255
+    if (c < 0) {
+      c = 0
+      if (!this.options.clampLow) {
+        alpha = 0
+      }
     }
 
-    public static rgbToHex(r: number, g: number, b: number): string {
-        const rgb = [r, g, b]
-
-        function _componentToHex(c: number) {
-            const hex = c.toString(16)
-            return hex.length === 1 ? '0' + hex : hex
-        }
-
-        return '#' + rgb.map(_componentToHex).join('')
+    if (c > this.options.width - 1) {
+      c = this.options.width - 1
+      if (!this.options.clampHigh) {
+        alpha = 0
+      }
     }
 
-    public static hexToRgb(hexWithPrefix: string): number[] {
-        const hex = hexWithPrefix.slice(1)
-        const arrBuff = new ArrayBuffer(4)
-        const vw = new DataView(arrBuff)
-        vw.setUint32(0, parseInt(hex, 16), false)
-        const arrByte = new Uint8Array(arrBuff)
+    const A = alpha
+    const R = this.csImageData[c * 4]
+    const G = this.csImageData[c * 4 + 1]
+    const B = this.csImageData[c * 4 + 2]
 
-        const r = arrByte[1]
-        const g = arrByte[2]
-        const b = arrByte[3]
-        return [r, g, b]
+    return [R, G, B, A]
+  }
+
+  public flip(): ColorScale {
+    const newDefinition = { ...{}, ...this.definition }
+    newDefinition.colors = this.definition.colors.reverse() // << flip color order from original
+
+    const flipped = new ColorScale(newDefinition, this.options) // with same options
+    return flipped
+  }
+
+  private _prepareScale(): void {
+    const c = this._createCanvas()
+    const ctx = c.getContext('2d')
+    this._createGradientIn(ctx)
+
+    // this.canvas = c
+    this.csImageData = ctx.getImageData(0, 0, this.options.width, 1).data
+  }
+
+  private _createCanvas(): HTMLCanvasElement {
+    const c = new HTMLCanvasElement()
+    c.width = this.options.width
+    c.height = this.options.height
+    return c
+  }
+
+  private _createGradientIn(ctx: CanvasRenderingContext2D): void {
+    const g = ctx.createLinearGradient(0, 0, this.options.width, 0)
+
+    // draw linear gradient with colors and stops from definition
+    const d = this.definition
+    for (let i = 0; i < d.colors.length; ++i) {
+      g.addColorStop(d.positions[i], d.colors[i])
     }
-
-    private static scales: { [n: string]: IScaleDefinition } = {
-        autumn: {
-            colors: ['#ff0000', '#ffff00'],
-            positions: [0, 1]
-        },
-        blackbody: {
-            colors: ['#00000000', '#e60000', '#e6d200', '#ffffff', '#a0c8ff'],
-            positions: [0, 0.2, 0.4, 0.7, 1]
-        },
-        bluered: {
-            colors: ['#0000ff', '#ff0000'],
-            positions: [0, 1]
-        },
-        bone: {
-            colors: ['#000000', '#545474', '#a9c8c8', '#ffffff'],
-            positions: [0, 0.376, 0.753, 1]
-        },
-        cool: {
-            colors: ['#00ffff', '#ff00ff'],
-            positions: [0, 1]
-        },
-        copper: {
-            colors: ['#000000', '#ffa066', '#ffc77f'],
-            positions: [0, 0.804, 1]
-        },
-        earth: {
-            colors: ['#000082', '#00b4b4', '#28d228', '#e6e632', '#784614', '#ffffff'],
-            positions: [0, 0.1, 0.2, 0.4, 0.6, 1]
-        },
-        electric: {
-            colors: ['#000000', '#1e0064', '#780064', '#a05a00', '#e6c800', '#fffadc'],
-            positions: [0, 0.15, 0.4, 0.6, 0.8, 1]
-        },
-        greens: {
-            colors: ['#00441b', '#006d2c', '#238b45', '#41ab5d', '#74c476', '#a1d99b', '#c7e9c0', '#e5f5e0', '#f7fcf5'],
-            positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-        },
-        greys: {
-            colors: ['#000000', '#ffffff'],
-            positions: [0, 1]
-        },
-        hot: {
-            colors: ['#000000', '#e60000', '#ffd200', '#ffffff'],
-            positions: [0, 0.3, 0.6, 1]
-        },
-        hsv: {
-            colors: ['#ff0000', '#fdff02', '#f7ff02', '#00fc04', '#00fc0a', '#01f9ff', '#0200fd', '#0800fd', '#ff00fb', '#ff00f5', '#ff0006'],
-            positions: [0, 0.169, 0.173, 0.337, 0.341, 0.506, 0.671, 0.675, 0.839, 0.843, 1]
-        },
-        ica: {
-            colors: ['#256814', '#40bd27', '#bfbf1f', '#8f201e', '#f40e05'],
-            positions: [0, 0.25, 0.5, 0.75, 1]
-        },
-        jet: {
-            colors: ['#000083', '#003CAA', '#05FFFF', '#FFFF00', '#FA0000', '#800000'],
-            positions: [0, 0.125, 0.375, 0.625, 0.875, 1]
-        },
-        lden: {
-            colors: ['#fafafa22', '#ffff02', '#ffcd69', '#ff8002', '#ff0202', '#ff00ff'],
-            positions: [45, 50, 55, 60, 65, 70, 75, 80]
-        },
-        picnic: {
-            colors: ['#0000ff', '#3399ff', '#66ccff', '#99ccff', '#ccccff', '#ffffff', '#ffccff', '#ff99ff', '#ff66cc', '#ff6666', '#ff0000'],
-            positions: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-        },
-        portland: {
-            colors: ['#0c3383', '#0a88ba', '#f2d338', '#f28f38', '#d91e1e'],
-            positions: [0, 0.25, 0.5, 0.75, 1]
-        },
-        rainbow: {
-            colors: ['#96005A', '#0000C8', '#0019FF', '#0098FF', '#2CFF96', '#97FF00', '#FFEA00', '#FF6F00', '#FF0000'],
-            positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-        },
-        rdbu: {
-            colors: ['#050aac', '#6a89f7', '#bebebe', '#dcaa84', '#e6915a', '#b20a1c'],
-            positions: [0, 0.35, 0.5, 0.6, 0.7, 1]
-        },
-        ruido: {
-            colors: ['#00ff0022', '#00ff00', '#008000', '#ffff00', '#a07f00', '#ff7f00', '#ff7f7f', '#ff0000', '#a00020', '#7f00a0', '#3f00ff'],
-            positions: [40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80]
-        },
-        spring: {
-            colors: ['#ff00ff', '#ffff00'],
-            positions: [0, 1]
-        },
-        summer: {
-            colors: ['#008066', '#ffff66'],
-            positions: [0, 1]
-        },
-        troposfera: {
-            colors: ['rgba(128,128,128,0)',
-                'rgb(79,9,207)',
-                'rgb(8,55,206)',
-                'rgb(8,184,216)',
-                'rgb(14,234,138)',
-                'rgb(20,229,9)',
-                'rgb(139,214,8)',
-                'rgb(248,193,26)',
-                'rgb(243,56,19)',
-                'rgb(219,9,103)'
-            ]
-        },
-        viridis: {
-            colors: ['#440154', '#482777', '#3f4a8a', '#31678e', '#26838f', '#1f9d8a', '#6cce5a', '#b6de2b', '#fee825']
-        },
-        winter: {
-            colors: ['#0000ff', '#00ff80'],
-            positions: [0, 1]
-        },
-        yignbu: {
-            colors: ['#081d58', '#253494', '#225ea8', '#1d91c0', '#41b6c4', '#7fcdbb', '#c7e9b4', '#edf8d9', '#ffffd9'],
-            positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-        },
-        yiorrd: {
-            colors: ['#800026', '#bd0026', '#e31a1c', '#fc4e2a', '#fd8d3c', '#feb24c', '#fed976', '#ffeda0', '#ffffcc'],
-            positions: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-        },
-    }
-
-    // private canvas: HTMLCanvasElement
-    private csImageData: Uint8ClampedArray
-    private definition: IScaleDefinition
-
-    private options: IColorScaleOptions = {
-        clampHigh: false,
-        clampLow: false,
-        domain: [0, 1],
-        height: 1,
-        width: 256,
-    }
-
-    constructor(definition: IScaleDefinition, options?: IColorScaleOptions) {
-        super()
-        Util.setOptions(this, options)
-
-        this.definition = ColorScale.buildFullDefinition(definition)
-        this._prepareScale()
-    }
-
-    public setDomain(d: number[]) {
-        this.options.domain = d
-        return this
-    }
-
-    public colorFor(value: number) {
-        const [min, max] = this.options.domain
-        const range = max - min
-
-        let c = Math.round((this.options.width - 1) * ((value - min) / range))
-
-        // clamp values?
-        let alpha = 255
-        if (c < 0) {
-            c = 0
-            if (!this.options.clampLow) {
-                alpha = 0
-            }
-        }
-
-        if (c > this.options.width - 1) {
-            c = this.options.width - 1
-            if (!this.options.clampHigh) {
-                alpha = 0
-            }
-        }
-
-        const A = alpha
-        const R = this.csImageData[c * 4]
-        const G = this.csImageData[c * 4 + 1]
-        const B = this.csImageData[c * 4 + 2]
-
-        return [R, G, B, A]
-    }
-
-    public flip(): ColorScale {
-        const newDefinition = { ...{}, ...this.definition }
-        newDefinition.colors = this.definition.colors.reverse() // << flip color order from original
-
-        const flipped = new ColorScale(newDefinition, this.options) // with same options
-        return flipped
-    }
-
-    private _prepareScale(): void {
-        const c = this._createCanvas()
-        const ctx = c.getContext('2d')
-        this._createGradientIn(ctx)
-
-        // this.canvas = c
-        this.csImageData = ctx.getImageData(0, 0, this.options.width, 1).data
-    }
-
-    private _createCanvas(): HTMLCanvasElement {
-        const c = new HTMLCanvasElement()
-        c.width = this.options.width
-        c.height = this.options.height
-        return c
-    }
-
-    private _createGradientIn(ctx: CanvasRenderingContext2D): void {
-        const g = ctx.createLinearGradient(0, 0, this.options.width, 0)
-
-        // draw linear gradient with colors and stops from definition
-        const d = this.definition
-        for (let i = 0; i < d.colors.length; ++i) {
-            g.addColorStop(d.positions[i], d.colors[i])
-        }
-        ctx.fillStyle = g
-        ctx.fillRect(0, 0, this.options.width, this.options.height)
-    }
+    ctx.fillStyle = g
+    ctx.fillRect(0, 0, this.options.width, this.options.height)
+  }
 }
--- src/layer/L.GridLayer.ScalarGrid.ts
+++ src/layer/L.GridLayer.ScalarGrid.ts
@@ -5,126 +5,128 @@ import { ColorScale } from '../colorscale/L.ColorScale'
 import { ScalarGrid } from '../grid/ScalarGrid'
 
 export interface IScalarParameter {
-    nome: string
-    unidade: string
+  nome: string
+  unidade: string
 }
 
 export interface ITropGridLayerOptions extends GridLayerOptions {
-    color?: Scale
-    domain?: number[]
-    interpolate?: boolean
-    parametro?: IScalarParameter
-    pixelStep?: number
-    type?: string
+  color?: Scale
+  domain?: number[]
+  interpolate?: boolean
+  parametro?: IScalarParameter
+  pintarMinimos?: boolean
+  pixelStep?: number
+  type?: string
 }
 
 export class TropGridLayer extends GridLayer {
 
-    protected options: ITropGridLayerOptions
-    private _grid: ScalarGrid
+  protected options: ITropGridLayerOptions
+  private _grid: ScalarGrid
 
-    constructor(datos: ScalarGrid, ops?: ITropGridLayerOptions) {
-        super(ops)
-        this._grid = datos
-        Util.setOptions(this, ops)
-        this.options.bounds = this.getBounds()
-        this.options.color = ops.color || chroma.scale(ColorScale.getScale('troposfera').colors)
-        this.options.color.domain(this._grid.range)
-        this.options.domain = this._grid.range
-        this.options.interpolate = ops.interpolate !== undefined ? ops.interpolate : true
-        this.options.parametro = ops.parametro
-        this.options.pixelStep = ops.pixelStep || 2
-        this.options.type = ops.type || 'colormap'
+  constructor(datos: ScalarGrid, ops?: ITropGridLayerOptions) {
+    super(ops)
+    this._grid = datos
+    Util.setOptions(this, ops)
+    this.options.bounds = this.getBounds()
+    this.options.color = ops.color || chroma.scale(ColorScale.getScale('troposfera').colors)
+    this.options.color.domain(this._grid.range)
+    this.options.domain = this._grid.range
+    this.options.interpolate = ops.interpolate !== undefined ? ops.interpolate : true
+    this.options.parametro = ops.parametro
+    this.options.pixelStep = ops.pixelStep || 2
+    this.options.pintarMinimos = ops.pintarMinimos || false
+    this.options.type = ops.type || 'colormap'
+  }
+
+  public get dominio(): number[] { return this.options.domain }
+  public set dominio(rango: number[]) {
+    this.options.domain = rango
+    this.options.color.domain(this.options.domain)
+    this.redraw()
+  }
+
+  public get parametro(): IScalarParameter { return this.options.parametro }
+  public set parametro(p: IScalarParameter) { this.options.parametro = p }
+
+  public getBounds(): LatLngBounds {
+    const bb = this._grid.extent()
+
+    const southWest = new LatLng(bb[1], bb[0])
+    const northEast = new LatLng(bb[3], bb[2])
+    const bounds = new LatLngBounds(southWest, northEast)
+    return bounds
+  }
+
+  public getColor(): Scale { return this.options.color }
+  public setColorScale(nome: string, classes?: number | number[] | string[]) {
+    this.options.color = chroma.scale(ColorScale.getScale(nome).colors).domain(this.options.domain)
+    if (classes) {
+      if (classes instanceof Array && typeof classes[0] === 'string') {
+        this.options.color.classes(classes.length)
+      } else {
+        this.options.color.classes(classes as number | number[])
+      }
     }
+    this.redraw()
+  }
 
-    public get dominio(): number[] { return this.options.domain }
-    public set dominio(rango: number[]) {
-        this.options.domain = rango
-        this.options.color.domain(this.options.domain)
-        this.redraw()
-    }
+  public createTile(coords: Coords, done: DoneCallback): HTMLElement {
+    const tile = DomUtil.create('canvas', 'leaflet-tile leaflet-tile-loaded') as HTMLCanvasElement
+    const ctx = tile.getContext('2d')
+    const size = this.getTileSize()
+    tile.width = size.x
+    tile.height = size.y
 
-    public get parametro(): IScalarParameter { return this.options.parametro }
-    public set parametro(p: IScalarParameter) { this.options.parametro = p }
+    setTimeout(() => {
+      // console.log(coords)
+      const img = ctx.createImageData(tile.width, tile.height)
+      this._prepareImageIn(img.data, coords, size)
+      ctx.putImageData(img, 0, 0)
+      // tile.style.outline = '1px solid red'
+      done(new Error(''), tile)
+    }, 0)
+    return tile
+  }
 
-    public getBounds(): LatLngBounds {
-        const bb = this._grid.extent()
+  /**
+   * Prepares the image in data, as array with RGBAs
+   * [R1, G1, B1, A1, R2, G2, B2, A2...]
+   * private
+   * param {[[Type]]} data   [[RGBA vector]]
+   * param {Number} width
+   * param {Number} height
+   */
+  private _prepareImageIn(data: Uint8ClampedArray, coords: Coords, size: Point, overstep?: number) {
+    const scaledCoords = coords.scaleBy(this.getTileSize())
+    const i0 = scaledCoords.x
+    const j0 = scaledCoords.y
+    const step = overstep || this.options.pixelStep
+    const w4 = 4 * size.x
+    const f = (this.options.interpolate ? this._grid.interpolatedValueAt : this._grid.valueAt).bind(this._grid)
+    for (let j = 0; j < size.y; j += step) {
+      const pj = j + j0
+      for (let i = 0; i < size.x; i += step) {
+        const pi = i + i0
+        // const pointCoords = this._map.containerPointToLatLng([i, j])
+        const pointCoords = this._map.unproject([pi, pj], coords.z)
+        const v: number = f(pointCoords.lng, pointCoords.lat) // 'valueAt' | 'interpolatedValueAt'
 
-        const southWest = new LatLng(bb[1], bb[0])
-        const northEast = new LatLng(bb[3], bb[2])
-        const bounds = new LatLngBounds(southWest, northEast)
-        return bounds
-    }
-
-    public getColor(): Scale { return this.options.color }
-    public setColorScale(nome: string, classes?: number | number[] | string[]) {
-        this.options.color = chroma.scale(ColorScale.getScale(nome).colors).domain(this.options.domain)
-        if (classes) {
-            if (classes instanceof Array && typeof classes[0] === 'string') {
-                this.options.color.classes(classes.length)
-            } else {
-                this.options.color.classes(classes as number | number[])
+        if (v !== null && (this.options.pintarMinimos || v >= this.options.domain[0])) {
+          const pos0 = 4 * (j * size.x + i)
+          const [R, G, B, A] = this.options.color(v).rgba() // this._getColorFor(v)
+          for (let sx = 0; sx < step && sx + i < size.x; sx++) {
+            const pos1 = pos0 + 4 * sx
+            for (let sy = 0; sy < step && j + sy < size.y; sy++) {
+              const pos = pos1 + w4 * sy
+              data[pos] = R
+              data[pos + 1] = G
+              data[pos + 2] = B
+              data[pos + 3] = Math.floor(A * 255) // not percent in alpha but hex 0-255
             }
+          }
         }
-        this.redraw()
+      }
     }
-
-    public createTile(coords: Coords, done: DoneCallback): HTMLElement {
-        const tile = DomUtil.create('canvas', 'leaflet-tile leaflet-tile-loaded') as HTMLCanvasElement
-        const ctx = tile.getContext('2d')
-        const size = this.getTileSize()
-        tile.width = size.x
-        tile.height = size.y
-
-        setTimeout(() => {
-            // console.log(coords)
-            const img = ctx.createImageData(tile.width, tile.height)
-            this._prepareImageIn(img.data, coords, size)
-            ctx.putImageData(img, 0, 0)
-            // tile.style.outline = '1px solid red'
-            done(new Error(''), tile)
-        }, 0)
-        return tile
-    }
-
-    /**
-     * Prepares the image in data, as array with RGBAs
-     * [R1, G1, B1, A1, R2, G2, B2, A2...]
-     * private
-     * param {[[Type]]} data   [[RGBA vector]]
-     * param {Number} width
-     * param {Number} height
-     */
-    private _prepareImageIn(data: Uint8ClampedArray, coords: Coords, size: Point, overstep?: number) {
-        const scaledCoords = coords.scaleBy(this.getTileSize())
-        const i0 = scaledCoords.x
-        const j0 = scaledCoords.y
-        const step = overstep || this.options.pixelStep
-        const w4 = 4 * size.x
-        const f = (this.options.interpolate ? this._grid.interpolatedValueAt : this._grid.valueAt).bind(this._grid)
-        for (let j = 0; j < size.y; j += step) {
-            const pj = j + j0
-            for (let i = 0; i < size.x; i += step) {
-                const pi = i + i0
-                // const pointCoords = this._map.containerPointToLatLng([i, j])
-                const pointCoords = this._map.unproject([pi, pj], coords.z)
-                const v: number = f(pointCoords.lng, pointCoords.lat) // 'valueAt' | 'interpolatedValueAt'
-
-                if (v && v >= this.options.domain[0]) {
-                    const pos0 = 4 * (j * size.x + i)
-                    const [R, G, B, A] = this.options.color(v).rgba() // this._getColorFor(v)
-                    for (let sx = 0; sx < step && sx + i < size.x; sx++) {
-                        const pos1 = pos0 + 4 * sx
-                        for (let sy = 0; sy < step && j + sy < size.y; sy++) {
-                            const pos = pos1 + w4 * sy
-                            data[pos] = R
-                            data[pos + 1] = G
-                            data[pos + 2] = B
-                            data[pos + 3] = Math.floor(A * 255) // not percent in alpha but hex 0-255
-                        }
-                    }
-                }
-            }
-        }
-    }
+  }
 }
